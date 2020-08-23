b2534dd47b646012b5723985e280654634140a16
Robin Giese <robin@grumpycorp.com>
2020-08-23 16:47:00 -0700

    Fix test break

--- packages/firmware/slider/tests/main.cpp
+++ packages/firmware/slider/tests/main.cpp
@@ -1,10 +1,6 @@
 #define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this here
 #include "base.h"
 
-// Include implementation source files
-#include "../Thermostat.cpp"
-#include "../ThermostatSetpointScheduler.cpp"
-
 // Instantiate mock globals
 MockEEPROM EEPROM;
 MockParticle Particle;
