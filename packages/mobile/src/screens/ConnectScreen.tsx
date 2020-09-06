5352f73e2a39e5077c1c65e3a58a4d8d1e664cf8
Robin Giese <robin@grumpycorp.com>
2020-09-06 15:10:40 -0700

    Fix up screens

--- packages/mobile/src/screens/ConnectScreen.tsx
+++ packages/mobile/src/screens/ConnectScreen.tsx
@@ -1,45 +1,62 @@
-import { Button, Surface, Text, Title } from "react-native-paper";
+import { ActivityIndicator, Button, Surface, Title } from "react-native-paper";
 
 import BaseView from "../components/BaseView";
 import GrumpyRobin from "../assets/grumpy-robin.svg";
 import { NavigationStackScreenComponent } from "react-navigation-stack";
 import React from "react";
+import ScreenRoutes from "./ScreenRoutes";
 import { View } from "react-native";
 import { observer } from "mobx-react";
 import { useRootStore } from "../stores/RootStoreContext";
 
-const ConnectScreen: NavigationStackScreenComponent<{}> = (): React.ReactElement => {
-  const rootStore = useRootStore();
+const ConnectScreen: NavigationStackScreenComponent<{}> = ({ navigation }): React.ReactElement => {
+  const bluetoothStore = useRootStore().bluetoothStore;
 
-  return (
-    <BaseView>
-      <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-        <Surface
-          style={{
-            justifyContent: "center",
-            alignItems: "center",
-            width: "100%",
-            height: 80,
-            marginBottom: 20,
-          }}
-        >
-          <GrumpyRobin fill="#000" height={60} width={60} />
-        </Surface>
-        <Title style={{ marginBottom: 20 }}>Camera Slider</Title>
-        <Text>{rootStore.bluetoothStore.deviceStatus}</Text>
-        <Button
-          mode="contained"
-          onPress={async (): Promise<void> => {
-            //await authStore.authProvider.requestLogin();
-            console.log("Mango");
-            rootStore.bluetoothStore.connect();
-          }}
-        >
-          Sign in
-        </Button>
-      </View>
-    </BaseView>
-  );
+  if (bluetoothStore.state === "initializing" || bluetoothStore.state === "connecting") {
+    return (
+      <BaseView>
+        <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+          <ActivityIndicator animating />
+        </View>
+      </BaseView>
+    );
+  } else if (bluetoothStore.state === "disconnected") {
+    return (
+      <BaseView>
+        <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+          <Surface
+            style={{
+              justifyContent: "center",
+              alignItems: "center",
+              width: "100%",
+              height: 80,
+              marginBottom: 20,
+            }}
+          >
+            <GrumpyRobin fill="#000" height={60} width={60} />
+          </Surface>
+          <Title style={{ marginBottom: 20 }}>Camera Slider</Title>
+          <Button
+            mode="contained"
+            onPress={(): void => {
+              bluetoothStore.connect();
+            }}
+          >
+            Connect
+          </Button>
+        </View>
+      </BaseView>
+    );
+  } else if (bluetoothStore.state === "connected") {
+    setImmediate(() => {
+      // Can only run this once render is done
+      navigation.navigate(ScreenRoutes.Home);
+    });
+
+    return <BaseView />;
+  } else {
+    throw new Error("Unexpected authentication state");
+  }
 };
 
 export default observer(ConnectScreen);
--- packages/mobile/src/screens/HomeScreen.tsx
+++ packages/mobile/src/screens/HomeScreen.tsx
@@ -6,6 +6,8 @@ import BaseView from "../components/BaseView";
 import React from "react";
 import ScreenProps from "./ScreenProps";
 import ScreenRoutes from "./ScreenRoutes";
+import { observer } from "mobx-react";
+import { useRootStore } from "../stores/RootStoreContext";
 
 /* ESLint believes the headerRight created below is a component. Alas. */
 /* eslint-disable react/no-multi-comp */
@@ -14,10 +16,21 @@ import ScreenRoutes from "./ScreenRoutes";
 /* This NavigationStack stuff is too weird to mess with it. Alas. */
 /* eslint-disable react/function-component-definition */
 
-const HomeScreen: NavigationStackScreenComponent<{}> = (): React.ReactElement => {
+const HomeScreen: NavigationStackScreenComponent<{}> = ({ navigation }): React.ReactElement => {
+  const bluetoothStore = useRootStore().bluetoothStore;
+
+  if (!bluetoothStore.isConnected) {
+    setImmediate(() => {
+      // Can only run this once render is done
+      navigation.navigate(ScreenRoutes.Connect);
+    });
+
+    return <BaseView />;
+  }
+
   return (
     <BaseView>
-      <Text>Hello.</Text>
+      <Text>{bluetoothStore.deviceStatus}</Text>
     </BaseView>
   );
 };
@@ -43,4 +56,4 @@ HomeScreen.navigationOptions = ({
   },
 });
 
-export default HomeScreen;
+export default observer(HomeScreen);
