a1422ac39a44baa360df4247fbd98ac8c51409e9
Robin Giese <robin@grumpycorp.com>
2023-12-19 13:52:42 -0800

    Retire mobile app

--- packages/mobile/src/screens/ScreenProps.ts
+++
@@ -1,7 +0,0 @@
-import { Theme } from "react-native-paper";
-
-// Used to pass the theme to navigationOptions/defaultNavigationOptions where it isn't available from withTheme(),
-// e.g. inside a static navigationOptions lambda.
-export default interface ScreenProps {
-  theme: Theme;
-}
--- packages/shared/src/index.ts
+++
@@ -1,6 +0,0 @@
-export * from "./BluetoothServices";
-export * from "./MovementProgram";
-export * from "./SliderStates";
-
-export * from "./generated/firmware_generated";
-export { flatbuffers } from "./flatbuffers";
